6a27be21ddb410c6cebf82b4b4768995b15cfbb6
S C <choksey2022@gmail.com>
2025-03-24 17:30:25 -0700

    config folder created

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("hello world!");
-})
+app.get("/products", (req, res) => {});
+
+console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => console.log("server is running at http://localhost:5000"));   
 
