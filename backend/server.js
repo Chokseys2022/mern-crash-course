218297b4d38b6bc833a061c748d61f1d11c44067
S C <choksey2022@gmail.com>
2025-03-25 16:18:21 -0700

    endpoints post and delete done

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,16 @@ dotenv.config();
 const app = express();
 app.use(express.json()); // Middleware to parse JSON data
 
+app.get("api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.error("Error fetching products:", error.message);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
 // Connect to the database before starting the server
 connectDB();
 
@@ -28,6 +38,19 @@ app.post("/api/products", async (req, res) => {
   } catch (error) {
     console.error("Error creating product:", error.message);
     res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res
+      .status(200)
+      .json({ success: true, message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Product not found" });
   }
 });
 
