29dc5b8273c5db48ff7c29b17f05a315d0f3e64c
S C <choksey2022@gmail.com>
2025-03-24 17:21:02 -0700

    mongokey added

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,13 @@
 import express from "express";
 
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hello world!");
+})
+
 app.listen(5000, () => console.log("server is running at http://localhost:5000"));   
+
+
+
+
