58470d56c867ae768584d68848f3cc7b52d83f1d
S C <choksey2022@gmail.com>
2025-03-26 10:51:01 -0700

    PORT added to env

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,8 @@ app.use("/api/products",productsRoutes )
 connectDB()
 
 //**** Start the server on the specified port and log a message ****//
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`); // Log server start message
 });
